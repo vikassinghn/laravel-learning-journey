1cd97f55163e90f8fe6724b2b6a4d47cd7afa4f5
vikassinghn <vikassingh227815@gmail.com>
2025-11-01 23:07:37 +0530

    Added Day 4: Eloquent Model Conventions

---
+++ Day4_Model_Conventions/app/Models/Student.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+    protected $table = "my_table";
+
+    protected $primaryKey = "student_id";
+
+    public $timestamps = false;
+}
