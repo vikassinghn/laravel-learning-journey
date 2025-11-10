3ce730df6e973204b1f78f88448579c91454adcf
vikassinghn <vikassingh227815@gmail.com>
2025-11-10 17:47:05 +0530

    Added Day 11: Tested Eloquent Relationships (One-to-One, One-to-Many, and Polymorphic)

---
+++ Day11_Test_Some_Relationships/app/Http/Controllers/PostController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $post = Post::with(["user" => function ($query) {
+            $query->select("id", "name", "email");
+        }])->get(["title", "description", "user_id"]);
+        // $post = Post::with("user:name as User Name,email as User Email,id")->get(["title", "description", "user_id"]);
+
+        return $post;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ Day11_Test_Some_Relationships/app/Http/Controllers/UserController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // $user = User::where("city", "Delhi")
+        //     ->get(["name", "email", "city"]);
+
+        // $user = User::all("name", "email")->toArray();
+
+        // $user = User::pluck("email", "name");
+
+        // $user = User::find(1, ["name", "email"]);
+
+        // $user = User::find(1)->email;
+
+        $user = User::all();
+
+        $user->load("post");
+
+        return $user;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
